0359018c45827c30ebf8812aa279833f3a1e3d3f
pulocks <pulock_kuet@yahoo.com>
2020-05-07 16:19:34 +0600

    hot-gadget-001 commited

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('smartphone').addEventListener('click', () => {
+    alert('More smartphones are coming soon.........');
+})
+
+document.getElementById('laptop').addEventListener('click', () => {
+    alert('More Laptops are coming soon.........');
+})
